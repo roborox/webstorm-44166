59cb162715870b18fbf0ce04e90e733ca99d0434
Roborox dev <dev@roborox.org>
2020-03-03 23:48:35 +0700

    demonstrate issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function hoc<TProps>(Component: React.ComponentType<TProps>): (props: TProps) => React.ReactElement | null {
+  return (props) => <Component {...props}/>
+}
+
+interface Props {
+  value: string
+}
+
+const Test = ({value}: Props) => {
+  return <span>{value}</span>
+}
+
+const Hoc = hoc(Test)
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Hoc value="awd"/>;
 }
 
 export default App;
